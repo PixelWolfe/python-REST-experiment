eaccd8da9058af4cc33cccc3c03b073d15f5835e
Rob Wolfe <robertejohnson10@gmail.com>
2020-08-19 16:22:21 -0500

    Added home and info html pages, and render_template

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return render_template('home.html')
+
+@app.route('/info')
+def info():
+    return render_template('info.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
